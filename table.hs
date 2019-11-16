b8abd5ec3804e19b76640abff7975f1c235a9225
lucasmoutinho <lucas.silvamoutinho@gmail.com>
2019-11-16 14:13:29 -0300

    initialize board and set value

--- table.hs
+++ table.hs
@@ -1,7 +1,22 @@
-import qualified Data.Map
+import qualified Data.Map as Map -- Importa a estrutura de dados Map que consiste de (key, value)
 
-data Cell = Empty | Black | White deriving(Eq, Show)
+data Piece = Empty | Black | White deriving(Eq, Show)
 type Position = (Int,Int)
+type Cell = (Position, Piece)
+type Board = Map.Map Position Piece
 
-createTable :: Cell -> [(Position,Cell)]
+-- Cria tabuleiro com peças vazias
+createTable :: Piece -> [Cell]
 createTable z = [((x,y),z) | x <- [0..7] , y <- [0..7]]
+
+-- Inicia o tabuleiro com as peças iniciais
+-- O Map.union prioriza os valores da key da esquerda
+initializeBoard :: Board
+initializeBoard = Map.union (Map.fromList [((3, 3), White), ((4, 4), White), ((3, 4), Black), ((4, 3), Black)]) (Map.fromList (createTable Empty))
+
+-- Troca uma posição vazia por uma peça preta
+setBlack :: Position -> Board -> Board 
+setBlack pos board = if board Map.! pos == Empty then
+                        Map.union (Map.fromList [(pos,Black)]) (board)
+                    else
+                        board
